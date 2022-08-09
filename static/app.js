573c4b7ee8c01b0bb244d2c9b4a4eb5a77c9dcec
Billyhrussell <billy.hrussell@gmail.com>
2022-08-08 18:13:27 -0700

    fin

--- static/app.js
+++ static/app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const BASE_URL = "http://localhost:5001"
+const BASE_URL = "http://localhost:5000"
 
 const $listCupcakes = $("#listcupcakes");
 const $form = $("#newCupcakeForm");
@@ -11,22 +11,20 @@ let cupcakes;
 async function getCupcakes() {
   let response = await axios.get("/api/cupcakes");
   cupcakes = response.data.cupcakes;
-
+  // TODO: separation of concerns
   displayCupcakes(cupcakes);
 }
 
 function displayCupcakes(cupcakes) {
   $listCupcakes.empty()
   for (let cupcake of cupcakes) {
-    const $li = $("<li>");
-    $li.text(`${cupcake.flavor} ${cupcake.size} ${cupcake.rating}`);
-    const $img = $(`<img src= ${cupcake.image}>`);
-    $li.append($img);
-    $listCupcakes.append($li);
+    let liHtml = generateCupcakeHtml(cupcake);
+    $listCupcakes.append(liHtml);
   }
 }
 
 async function handleSubmit(e) {
+ //handle submit button
   e.preventDefault();
 
   const response = await axios({
@@ -40,9 +38,21 @@ async function handleSubmit(e) {
     }
   });
   const cupcake = response.data;
-  getCupcakes(cupcake);
+  let liHtml = generateCupcakeHtml(cupcake.cupcake);
+  $listCupcakes.append(liHtml);
+  console.log(`add li html`, liHtml);
+  console.log("cupcake ", cupcake);
+}
 
+//TODO: another function to update with new cupcake
 
+function generateCupcakeHtml(cupcake){
+    const $li = $("<li>");
+    $li.text(`${cupcake.flavor} ${cupcake.size} ${cupcake.rating}`);
+    const $img = $(`<img src= ${cupcake.image}>`);
+    $li.append($img);
+
+    return $li;
 }
 
 $form.on("submit", handleSubmit);
