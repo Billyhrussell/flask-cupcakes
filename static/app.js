f8d692e9d4c9fff7bee46dfa3edec7af9ed8d75a
Margaret Sun <hi.margaret.sun@gmail.com>
2022-08-08 17:24:39 -0700

    pt 5

--- static/app.js
+++ static/app.js
@@ -1,27 +1,51 @@
-"use strict"
+"use strict";
 
-const $listCupcakes = $("#listcupcakes")
-const $form = $("#newCupcakeForm")
+const BASE_URL = "http://localhost:5001"
+
+const $listCupcakes = $("#listcupcakes");
+const $form = $("#newCupcakeForm");
 
 let cupcakes;
 
 
-async function getCupcakes(){
-  let response = await axios.post("/api/cupcakes");
-  cupcakes = response.data.cupcake;
+async function getCupcakes() {
+  let response = await axios.get("/api/cupcakes");
+  cupcakes = response.data.cupcakes;
 
   displayCupcakes(cupcakes);
 }
 
-function displayCupcakes(cupcakes){
-
-  for(let cupcake of cupcakes){
+function displayCupcakes(cupcakes) {
+  $listCupcakes.empty()
+  for (let cupcake of cupcakes) {
     const $li = $("<li>");
-    $li.text(`${cupcake.flavor} ${cupcake.size} ${cupcake.rating}`)
-    // const $img = $(`<img src= ${cupcake.image}>`)
-    // $li.append($img)
-    $listCupcakes.append($li)
+    $li.text(`${cupcake.flavor} ${cupcake.size} ${cupcake.rating}`);
+    const $img = $(`<img src= ${cupcake.image}>`);
+    $li.append($img);
+    $listCupcakes.append($li);
   }
 }
 
+async function handleSubmit(e) {
+  e.preventDefault();
+
+  const response = await axios({
+    url: `${BASE_URL}/api/cupcakes`,
+    method: "POST",
+    data: {
+      flavor: $("#flavor").val(),
+      size: $("#size").val(),
+      rating: $("#rating").val(),
+      image: $("#image").val()
+    }
+  });
+  const cupcake = response.data;
+  getCupcakes(cupcake);
+
+
+}
+
+$form.on("submit", handleSubmit);
+
 getCupcakes();
+
